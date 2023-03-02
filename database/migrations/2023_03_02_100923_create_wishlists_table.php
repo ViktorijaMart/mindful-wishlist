d05d235f41bfdffc8faa01c917d2bc817663c62f
LhunaCat <viktorijamartinaityte@gmail.com>
2023-03-02 13:07:35 +0200

    added columns to wishlist migration

--- database/migrations/2023_03_02_100923_create_wishlists_table.php
+++ database/migrations/2023_03_02_100923_create_wishlists_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('wishlists', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->string('name');
+            $table->decimal('price', $precision = 10, $scale = 2)->nullable();
+            $table->string('store_name')->nullable();
+            $table->string('store_url')->nullable();
+            $table->string('category');
+            $table->string('tags');
+            $table->time('reminder')->nullable();
+            $table->text('reflection')->nullable();
+            $table->boolean('bought')->default(0);
+            $table->boolean('graveyard')->default(0);
             $table->timestamps();
         });
     }
