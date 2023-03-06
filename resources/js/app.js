2a152dc1d51ae6c71df202898b0d6b56de79e483
LhunaCat <viktorijamartinaityte@gmail.com>
2023-03-06 18:47:38 +0200

    functional dropdown menu

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,26 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+console.log('hello')
+
+const dropdownBtn = document.getElementById('btn');
+const dropdownLinks = document.getElementById('dropdown_links');
+const toggleArrowIcon = document.getElementById('arrow_icon');
+
+const toggleDropdown = function () {
+    dropdownLinks.classList.toggle("show_dropdown");
+    toggleArrowIcon.classList.toggle("arrow_icon");
+    console.log('clicked')
+};
+
+dropdownBtn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    toggleDropdown();
+});
+
+document.documentElement.addEventListener("click", function () {
+    if (dropdownLinks.classList.contains("show_dropdown")) {
+        toggleDropdown();
+    }
+});
